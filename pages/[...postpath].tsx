eef9b9cf37f202012e80b3d7c9b3c51a1438e641
arthursantosinfo <135532114+arthursantosinfo@users.noreply.github.com>
2023-06-05 16:39:12 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://aslijob.com` + encodeURI(path as string)
+					`https://aslijob.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
